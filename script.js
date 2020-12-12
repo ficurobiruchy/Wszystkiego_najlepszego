8d0bed55b10f7ae5ba8e93f62d398ad22c46225b
ficurobiruchy <75887179+ficurobiruchy@users.noreply.github.com>
2020-12-12 14:58:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function open_card() {
+  document.getElementById('outside').className = 'open-card';
+}
+
+function close_card() {
+  document.getElementById('outside').className = '';
+}
